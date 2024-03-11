b86d2f13add09fc4fe3a1af3753e1710fc5f4bcd
samxvsn <59977376+VISION-SAM@users.noreply.github.com>
2024-03-11 16:02:17 +0530

    Add files via upload

---
+++ mac_changer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+def get_arg():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Assign a new mac address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify an mac, use --help for more info.")
+    return options
+
+def changemac(interface, new_mac):
+    print("[+] Changing MAC for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+options = get_arg()
+changemac(options.interface, options.new_mac)
+
